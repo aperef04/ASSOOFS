3060d966bdcee5aedb4c0b36562952a0c5e9f6ee
aperef04 <aperef04@estudiantes.unileon.es>
2020-05-19 12:51:02 +0200

    Funcion fill_supper

--- assoofs.c
+++ assoofs.c
@@ -6,6 +6,10 @@
 #include <linux/slab.h>         /* kmem_cache            */
 #include "assoofs.h"
 
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Alejandro Perez Fernandez");
+
 /*
  *  Operaciones sobre ficheros
  */
@@ -80,12 +84,75 @@ static const struct super_operations assoofs_sops = {
  */
 int assoofs_fill_super(struct super_block *sb, void *data, int silent) {   
     printk(KERN_INFO "assoofs_fill_super request\n");
-    // 1.- Leer la información persistente del superbloque del dispositivo de bloques  
+    // 1.- Leer la información persistente del superbloque del dispositivo de bloques
+    struct buffer_head *bh;
+    struct assoofs_super_block_info *assoofs_sb;
+
+    bh = sb_bread(sb, ASSOOFS_SUPERBLOCK_BLOCK_NUMBER); 
+    assoofs_sb = (struct assoofs_super_block_info *)bh->b_data; 
+
+    brelse(bh); //Liberar memoria
+
     // 2.- Comprobar los parámetros del superbloque
+    if(assoofs_sb->magic != ASSOOFS_MAGIC || assoofs_sb->block_size != ASSOOFS_DEFAULT_BLOCK_SIZE)
+        return -1;
+
     // 3.- Escribir la información persistente leída del dispositivo de bloques en el superbloque sb, incluído el campo s_op con las operaciones que soporta.
+    sb->s_magic = ASSOOFS_MAGIC;
+    sb->s_maxbytes = ASSOOFS_DEFAULT_BLOCK_SIZE;
+    sb->s_op = *assoofs_sops;
+    sb->s_fs_info = assoofs_sb;
+
     // 4.- Crear el inodo raíz y asignarle operaciones sobre inodos (i_op) y sobre directorios (i_fop)
+    struct inode *root_inode;
+    root_inode = new_inode(sb);
+    inode_init_owner(root_inode, NULL, S_IFDIR);
+    root_inode->i_ino = ASSOOFS_ROOTDIR_INODE_NUMBER; // numero de inodo
+    root_inode->i_sb = sb; // puntero al superbloque
+    root_inode->i_op = &assoofs_inode_ops; // direccion de una variable de tipo struct inode_operations previamente declarada
+    root_inode->i_fop = &assoofs_dir_operations; // direccion de una variable de tipo struct file_operations previamente declarada.
+                                                //En la practica tenemos 2: assoofs_dir_operations y assoofs_file_operations. La primera la utilizaremos 
+                                                //cuando creemos inodos para directorios (como el directorio ra´ız) y la segunda cuando creemos inodos para ficheros.
+    root_inode->i_atime = root_inode->i_mtime = root_inode->i_ctime = current_time(root_inode); // fechas.
+    root_inode->i_private = assoofs_get_inode_info(sb, ASSOOFS_ROOTDIR_INODE_NUMBER); // Informaci´on persistente del inodo
+
+    sb->s_root = d_make_root(root_inode); //Por ser el inodo raiz
+
     return 0;
 }
+
+
+
+
+struct assoofs_inode_info *assoofs_get_inode_info(struct super_block *sb, uint64_t inode_no){
+    struct assoofs_inode_info *inode_info = NULL;
+    struct buffer_head *bh;
+
+    bh = sb_bread(sb, ASSOOFS_INODESTORE_BLOCK_NUMBER);
+    inode_info = (struct assoofs_inode_info *)bh->b_data;
+
+
+    struct assoofs_super_block_info *afs_sb = sb->s_fs_info;
+    struct assoofs_inode_info *buffer = NULL;
+    int i;
+    for (i = 0; i < afs_sb->inodes_count; i++) {
+        if (inode_info->inode_no == inode_no) {
+            buffer = kmalloc(sizeof(struct assoofs_inode_info), GFP_KERNEL);
+            memcpy(buffer, inode_info, sizeof(*buffer));
+            break;
+        }
+    inode_info++;
+
+    }
+
+    brelse(bh);
+    return buffer;
+
+
+}
+
+
+
 
 /*
  *  Montaje de dispositivos assoofs
@@ -96,9 +163,9 @@ static struct dentry *assoofs_mount(struct file_system_type *fs_type, int flags,
     // Control de errores a partir del valor de ret. En este caso se puede utilizar la macro IS_ERR: if (IS_ERR(ret)) ...
     if (IS_ERR(ret)){
         printk(KERN_INFO "Error in assoofs_mount ");
-        return -1;
     }else{
          printk(KERN_INFO "assoofs_mount completed");
+         return ret;
     }
 }
 
@@ -118,21 +185,17 @@ static int __init assoofs_init(void) {
     // Control de errores a partir del valor de ret
     if(ret !=0){
         printk(KERN_INFO "Error initializing filesystem ");
-        return -1;
     }
     printk(KERN_INFO "assoofs_init completed");
-    return 0;
 }
 
 static void __exit assoofs_exit(void) {
     printk(KERN_INFO "assoofs_exit request\n");
     int ret = unregister_filesystem(&assoofs_type);
     if(ret !=0){
-        printk(KERN_INFO "Error in assoofs_exit ")
-        return -1;
+        printk(KERN_INFO "Error in assoofs_exit ");
     }
     printk(KERN_INFO "assoofs_exit completed");
-    return 0;
 
 }
 
